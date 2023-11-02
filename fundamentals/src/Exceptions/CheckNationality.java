dd1bd96df025d424dfb7360e5a0b67d46afa888d
LuckyBelieve <luckybelieve10@gmail.com>
2023-11-02 13:41:25 +0200

    userDefined exceptions

---
+++ fundamentals/src/Exceptions/CheckNationality.java
@@ -0,0 +1,24 @@
+package Exceptions;
+
+import java.util.Objects;
+
+public class CheckNationality {
+    public static void check(String country) throws NationalityException {
+
+        if(!Objects.equals(country, "Rwanda"))
+            throw new NationalityException("You are not a Rwandan");
+        else{
+            System.out.println("You are a rwandan");
+        }
+    };
+
+    public static void main(String[] ags){
+        try {
+            check("Rwanda");
+        }catch (NationalityException e){
+            System.out.println(e.toString());
+        }finally {
+            System.out.println("Thanks for applying");
+        }
+    };
+}
---
+++ fundamentals/src/Exceptions/NationalityException.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+public class NationalityException extends Exception{
+    public String message;
+    public NationalityException(String message) {
+        super(message);
+        this.message = message;
+    }
+    public String toString(){
+        return "Customized Exception " + message;
+    }
+}
